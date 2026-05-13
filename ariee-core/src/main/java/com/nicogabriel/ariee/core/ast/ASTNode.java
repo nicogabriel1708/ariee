6dc9f103175cf9f5d346166a59bb9c1d34f6027b
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2026-05-13 19:25:23 +0200

    feat: implement Iterable interface; create static nested class implementing pre order iterator; add stream methods (from iterator implementation)

--- ariee-core/src/main/java/com/nicogabriel/ariee/core/ast/ASTNode.java
+++ ariee-core/src/main/java/com/nicogabriel/ariee/core/ast/ASTNode.java
@@ -2,7 +2,14 @@ package com.nicogabriel.ariee.core.ast;
 
 import com.nicogabriel.ariee.core.visitor.ASTVisitor;
 
-public abstract class ASTNode {
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public abstract class ASTNode implements Iterable<ASTNode> {
 
     private final int position;
 
@@ -10,9 +17,65 @@ public abstract class ASTNode {
         this.position = position;
     }
 
+    public abstract <T> T accept(ASTVisitor<T> visitor);
+
+    public Stream<ASTNode> stream() {
+        return StreamSupport.stream(spliterator(), false);
+    }
+
+    public Stream<ASTNode> parallelStream() {
+        return StreamSupport.stream(spliterator(), true);
+    }
+
+    @Override
+    public Iterator<ASTNode> iterator() {
+        return new PreOrderIterator(this);
+    }
+
     public int getPosition() {
         return position;
     }
 
-    public abstract <T> T accept(ASTVisitor<T> visitor);
+    private static final class PreOrderIterator implements Iterator<ASTNode> {
+
+        private final Deque<ASTNode> stack = new ArrayDeque<>();
+
+        private PreOrderIterator(ASTNode root) {
+            if (root != null) {
+                stack.push(root);
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public ASTNode next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            ASTNode node = stack.pop();
+
+            if (node instanceof OperatorNode operatorNode) {
+                ASTNode left = operatorNode.getLeft();
+                ASTNode right = operatorNode.getRight();
+
+                // to maintain pre-order traversal, the right child is pushed before the left child
+                // due to the stack's LIFO behavior, the left child is popped before the right child
+
+                if (right != null) {
+                    stack.push(right);
+                }
+
+                if (left != null) {
+                    stack.push(left);
+                }
+            }
+
+            return node;
+        }
+    }
 }
