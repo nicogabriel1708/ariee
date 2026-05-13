417c49da89b83b0b254cf583e59c94abc95aceed
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2026-05-13 15:15:12 +0200

    feat: add token factory class for building tokens from chars/strings

---
+++ ariee-core/src/main/java/com/nicogabriel/ariee/core/token/TokenFactory.java
@@ -0,0 +1,16 @@
+package com.nicogabriel.ariee.core.token;
+
+import java.util.Optional;
+
+public final class TokenFactory {
+
+    public static Optional<Token> fromChar(char c, int position) {
+        return fromString(String.valueOf(c), position);
+    }
+
+    public static Optional<Token> fromString(String s, int position) {
+        Optional<Token> operator = Operator.fromString(s).map(o -> new OperatorToken(o, position));
+        Optional<Token> bracket = Bracket.fromString(s).map(b -> new BracketToken(b, position));
+        return operator.isPresent() ? operator : bracket;
+    }
+}
