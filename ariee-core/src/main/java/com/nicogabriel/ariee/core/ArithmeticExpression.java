7626321181d1213572014b935d89676e68e1686b
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2026-05-26 22:32:05 +0200

    fix(ArithmeticExpression): migrate from hashmap cache implementation to concurrent hashmap, ensuring thread-safety; check for null before casting result of evaluation; rename argument of overriden equals() method

--- ariee-core/src/main/java/com/nicogabriel/ariee/core/ArithmeticExpression.java
+++ ariee-core/src/main/java/com/nicogabriel/ariee/core/ArithmeticExpression.java
@@ -1,18 +1,19 @@
 package com.nicogabriel.ariee.core;
 
+import com.nicogabriel.ariee.core.exception.ArieeInternalException;
 import com.nicogabriel.ariee.core.internal.ast.ASTNode;
 import com.nicogabriel.ariee.core.internal.visitor.ASTVisitor;
 import com.nicogabriel.ariee.core.internal.visitor.EvaluatorVisitor;
 
 import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public final class ArithmeticExpression {
 
     private static final double EPSILON = 1e-10;
 
-    private final Map<Class<? extends ASTVisitor<?>>, Object> cache = new HashMap<>();
+    private final Map<Class<? extends ASTVisitor<?>>, Object> cache = new ConcurrentHashMap<>();
     private final ASTNode rootNode;
 
     ArithmeticExpression(ASTNode rootNode) {
@@ -28,20 +29,26 @@ public final class ArithmeticExpression {
     }
 
     public double evaluate() {
-        return (double) cache.computeIfAbsent(EvaluatorVisitor.class, _ -> rootNode.accept(new EvaluatorVisitor()));
+        Object result = cache.computeIfAbsent(EvaluatorVisitor.class, _ -> rootNode.accept(new EvaluatorVisitor()));
+
+        if (result == null) {
+            throw new ArieeInternalException("Evaluation of the expression unexpectedly resulted in null.");
+        }
+
+        return (double) result;
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
+    public boolean equals(Object object) {
+        if (this == object) {
             return true;
         }
 
-        if (obj == null || getClass() != obj.getClass()) {
+        if (object == null || getClass() != object.getClass()) {
             return false;
         }
 
-        ArithmeticExpression other = (ArithmeticExpression) obj;
+        ArithmeticExpression other = (ArithmeticExpression) object;
 
         return Math.abs(evaluate() - other.evaluate()) < EPSILON;
     }
