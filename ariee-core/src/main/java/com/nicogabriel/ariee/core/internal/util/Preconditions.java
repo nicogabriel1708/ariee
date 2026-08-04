5177ad90939d02a4b06306da59d1614f6a20bd4a
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2026-08-04 14:33:54 +0200

    refactor: add check methods with supplier arguments (for error message or default value) to Preconditions; remove Preconditions.check() to not construct unnecessary exceptions (because of eager argument evaluation)

--- ariee-core/src/main/java/com/nicogabriel/ariee/core/internal/util/Preconditions.java
+++ ariee-core/src/main/java/com/nicogabriel/ariee/core/internal/util/Preconditions.java
@@ -3,6 +3,7 @@ package com.nicogabriel.ariee.core.internal.util;
 import org.jspecify.annotations.Nullable;
 
 import java.util.Objects;
+import java.util.function.Supplier;
 
 public final class Preconditions {
 
@@ -11,50 +12,93 @@ public final class Preconditions {
     }
 
     public static <T> T checkArgumentNotNull(@Nullable T reference) {
-        return checkNotNull(reference, new IllegalArgumentException());
+        if (reference == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return reference;
     }
 
-    public static <T> T checkArgumentNotNull(@Nullable T reference, String errorMessage) {
-        return checkNotNull(reference, new IllegalArgumentException(errorMessage));
+    public static <T> T checkArgumentNotNull(@Nullable T reference, @Nullable String errorMessage) {
+        if (reference == null) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+
+        return reference;
+    }
+
+    public static <T> T checkArgumentNotNull(@Nullable T reference, @Nullable Supplier<String> errorMessageSupplier) {
+        if (reference == null) {
+            throw new IllegalArgumentException(errorMessageSupplier == null ? null : errorMessageSupplier.get());
+        }
+
+        return reference;
     }
 
     public static void checkArgument(boolean expression) {
-        check(expression, new IllegalArgumentException());
+        if (!expression) {
+            throw new IllegalArgumentException();
+        }
     }
 
-    public static void checkArgument(boolean expression, String errorMessage) {
-        check(expression, new IllegalArgumentException(errorMessage));
+    public static void checkArgument(boolean expression, @Nullable String errorMessage) {
+        if (!expression) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    public static void checkArgument(boolean expression, @Nullable Supplier<String> errorMessageSupplier) {
+        if (!expression) {
+            throw new IllegalArgumentException(errorMessageSupplier == null ? null : errorMessageSupplier.get());
+        }
     }
 
     public static void checkState(boolean expression) {
-        check(expression, new IllegalStateException());
+        if (!expression) {
+            throw new IllegalStateException();
+        }
     }
 
-    public static void checkState(boolean expression, String errorMessage) {
-        check(expression, new IllegalStateException(errorMessage));
+    public static void checkState(boolean expression, @Nullable String errorMessage) {
+        if (!expression) {
+            throw new IllegalStateException(errorMessage);
+        }
+    }
+
+    public static void checkState(boolean expression, @Nullable Supplier<String> errorMessageSupplier) {
+        if (!expression) {
+            throw new IllegalStateException(errorMessageSupplier == null ? null : errorMessageSupplier.get());
+        }
     }
 
     public static <T> T checkNotNull(@Nullable T reference) {
         return Objects.requireNonNull(reference);
     }
 
-    public static <T> T checkNotNull(@Nullable T reference, String errorMessage) {
+    public static <T> T checkNotNull(@Nullable T reference, @Nullable String errorMessage) {
         return Objects.requireNonNull(reference, errorMessage);
     }
 
-    public static <T> T checkNotNull(@Nullable T reference, RuntimeException runtimeException) {
-        check(reference != null, runtimeException);
-
-        return reference;
+    public static <T> T checkNotNull(@Nullable T reference, @Nullable Supplier<String> errorMessageSupplier) {
+        return Objects.requireNonNull(reference, errorMessageSupplier);
     }
 
-    public static <T> T checkNotNullElse(@Nullable T reference, T defaultValue) {
+    public static <T> T checkNotNullOrElse(@Nullable T reference, T defaultValue) {
         return Objects.requireNonNullElse(reference, defaultValue);
     }
 
-    private static void check(boolean expression, RuntimeException runtimeException) {
-        if (!expression) {
-            throw checkNotNullElse(runtimeException, new RuntimeException());
+    public static <T> T checkNotNullOrElseGet(@Nullable T reference, Supplier<T> defaultValueSupplier) {
+        return Objects.requireNonNullElseGet(reference, defaultValueSupplier);
+    }
+
+    public static <T> T checkNotNullOrElseThrow(
+            @Nullable T reference,
+            @Nullable Supplier<? extends RuntimeException> runtimeExceptionSupplier
+    ) {
+        if (reference == null) {
+            throw runtimeExceptionSupplier == null ? new RuntimeException() : runtimeExceptionSupplier.get();
         }
+
+        return reference;
     }
 }
