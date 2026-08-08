5ff75009a162138935bdaa02ef4f3e79ef931833
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2026-08-08 21:26:49 +0200

    feat: add helper methods to Token interface, determining of a token is of an (or any) expected type(s)

--- ariee-core/src/main/java/com/nicogabriel/ariee/core/internal/token/Token.java
+++ ariee-core/src/main/java/com/nicogabriel/ariee/core/internal/token/Token.java
@@ -1,8 +1,30 @@
 package com.nicogabriel.ariee.core.internal.token;
 
+import static com.nicogabriel.ariee.core.internal.util.Preconditions.checkArgumentNotNull;
+
 public interface Token {
 
     TokenType type();
 
     int position();
+
+    default boolean is(TokenType expectedType) {
+        checkArgumentNotNull(expectedType, "The given expected token type must not be null.");
+
+        return type() == expectedType;
+    }
+
+    default boolean isAny(TokenType... expectedTypes) {
+        checkArgumentNotNull(expectedTypes, "The given array of expected token types must not be null.");
+
+        for (TokenType expectedType : expectedTypes) {
+            checkArgumentNotNull(expectedType, "The given expected token type must not be null.");
+
+            if (is(expectedType)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
